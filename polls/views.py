047a39647783698a659c392feda928573f85862a
SHREYANSHBHALLA <shrey.bhalla123@gmail.com>
2024-10-18 18:23:33 -0400

    commentsspace

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
-#from django.template import loader
+# from django.template import loader
 from django.urls import reverse
 from django.views import generic
 
