6db57f492f0ca9669a97680d260a419290564fb1
SHREYANSHBHALLA <shrey.bhalla123@gmail.com>
2024-10-19 16:15:41 -0400

    T

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 # tests/test.py
 
+
 def add(a, b):
     return a + b
 
@@ -8,5 +9,6 @@ def test_add():
     assert add(1, 2) == 3
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+
 
 # Ensure there's a newline at the end of this file.
